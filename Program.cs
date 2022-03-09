5a3e382c70a8b4c6bcddee38dca00f656ba589b5
User 14 <User 14@PC15>
2022-03-09 14:38:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FractionsBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = { 7.8, 5.5, 4.5, 9.8, 1.3 };
+            double temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
